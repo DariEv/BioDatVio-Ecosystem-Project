14f42f3ed0a89fc7f6d7ae0c9b59d0a860c05599
Daria <daria.elena.evseeva@gmail.com>
2019-01-09 21:01:47 +0100

    switched heatmap axes, updated colormap

--- static/ecosystem1_heatmap.js
+++ static/ecosystem1_heatmap.js
@@ -7,7 +7,7 @@ function heatmapChart() {
 			console.log(taxa);
 			console.log("length", taxa.length);
 			
-			var samples = new Array(data.length); // create an empty array with length 45
+			var samples = new Array(data.length); // create an empty array
 			for(var i = 0; i < samples.length; i++){
 				samples[i] = + i;
 			}
@@ -16,68 +16,63 @@ function heatmapChart() {
 			console.log('max', max_of_array);
 			
 			
-			var margin = { top: 50, right: 0, bottom: 100, left: 300 },
+			var margin = { top: 250, right: 0, bottom: 100, left: 50 },
 			width = 10000 - margin.left - margin.right,
-			height = 2000 - margin.top - margin.bottom,
+			height = 10000 - margin.top - margin.bottom,
 			gridSize = Math.floor(width / (samples.length)),
 			legendElementWidth = gridSize*2,
 			buckets = 9,
-			colors = ["#ffffd9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8","#253494","#081d58"] // alternatively colorbrewer.YlGnBu[9]
+			colors = ["#ffffcc", "#c7e9b4", "#7fcdbb", "#41b6c4", "#2c7fb8", "#253494"]; //by colorbrewer: YlGnBu[6]
 
 			var svg = d3.select("#chart").append("svg")
 			  .attr("width", width + margin.left + margin.right)
 			  .attr("height", height + margin.top + margin.bottom)
 			  .append("g")
-			  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+			  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");			  			  
 			  
-			  
-			  
-			var taxaLabel = svg.selectAll(".taxaLabel")
-			  .data(taxa)
+			var samplesLabel = svg.selectAll(".samplesLabel")
+			  .data(samples)
 			  .enter().append("text")
 				.text(function (d) { return d; })
 				.attr("x", 0)
 				.attr("y", function (d, i) { return i * gridSize; })
 				.style("text-anchor", "end")
-				.attr("transform", "translate(-6," + gridSize / 1.5 + ")")
-				//.attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "taxaLabel mono axis axis-workweek" : "taxaLabel mono axis"); });
+				.attr("transform", "translate(-6," + gridSize / 1.5 + ")");
 
-			var sampleLabels = svg.selectAll(".sampleLabels")
-				  .data(samples)
-				  .enter().append("text")
-					.text(function(d) { return d; })
-					.attr("x", function(d, i) { return i * gridSize; })
-					.attr("y", 0)
-					.style("text-anchor", "middle")
-					.attr("transform", "translate(" + gridSize / 2 + ", -6)")
-					//.attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "sampleLabels mono axis axis-worktime" : "sampleLabels mono axis"); });
+			var taxaLabels = svg.selectAll(".taxaLabels")
+				  .data(taxa)
+				  .enter().append("text")			
+					.text(function(d) { return d; })					
+					.attr("y", function(d, i) { return i * gridSize; })
+					.attr("x", 0)
+					.style("text-anchor", "start")
+					.attr("transform", "translate(" + gridSize / 2 + ", -6) rotate(-90)");
 					
 			/*var colorScale = d3.scale.quantile()
               .domain([0, buckets - 1, d3.max(data, function (d) { return d.value; })])
               .range(colors);
 			*/
 
-          var cards = svg.selectAll(".sample")
-              .data(data);//, function(d) {console.log('fff', d); return d.taxa[i]+':'+d.samples[i];});
+			var cards = svg.selectAll(".sample")
+				.data(data);//, function(d) {return d.taxa[i]+':'+d.samples[i];});
 
-          cards.append("title");
+			cards.append("title");
 			
 			taxa.forEach(function(item, taxon) {
-			//console.log("n", Math.floor(Math.log10(Number(data[0][item]))))
 				cards.enter().append("rect")
-				  .attr("x", function(d, i) { return (i) * gridSize; })
-				  .attr("y", function(d, i) { return (taxon) * gridSize; })
-				  //.attr("rx", 4)
-				  //.attr("ry", 4)
+				  //.attr("x", function(d, i) { return (i) * gridSize; })
+				  //.attr("y", function(d, i) { return (taxon) * gridSize; })
+				  .attr("x", function(d, i) { return (taxon) * gridSize; })
+				  .attr("y", function(d, i) { return (i) * gridSize; })
 				  .attr("class", "hour bordered")
 				  .attr("width", gridSize)
 				  .attr("height", gridSize)
 				  .style("fill", function(d) {c = Math.floor(Math.log10(Number(d[item]))); 		
 												return colors[c];});
 			});
-		}
+			
+	}
 
-	returnDictionary['update']=function(){}
-
-        	return returnDictionary;  
-        };
+	//returnDictionary['update']=function(filtered_data){};
+    return returnDictionary;  
+};
