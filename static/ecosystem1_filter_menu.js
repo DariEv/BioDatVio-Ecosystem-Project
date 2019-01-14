280a85569737a85668f0dae5a377b7785ae52085
Sarah <sarah.ertel@gmx.de>
2019-01-14 15:09:59 +0100

    navigation active

--- static/ecosystem1_filter_menu.js
+++ static/ecosystem1_filter_menu.js
@@ -1,4 +1,25 @@
 $( document ).ready(function() {
+    
+    
+    if (window.location.href == "http://127.0.0.1:5000/project") {
+        
+        $("#proj").addClass("active")
+        
+    } else if (window.location.href == "http://127.0.0.1:5000/dataExploration") {
+        
+          $("#prof").addClass("active")
+        
+    } else if (window.location.href == "http://127.0.0.1:5000/metadataOverview") {
+        
+          $("#meta").addClass("active")
+        
+    } else {
+        
+          $("#pcoa").addClass("active")
+        
+    }
+    
+    //filter
     $(".dropdown-menu li a").on('click', function() {
 		$(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
 		$(this).parents(".dropdown").find('.btn').val($(this).attr('value'));
@@ -15,4 +36,15 @@ $( document ).ready(function() {
 		}
 		else{alert("Out of range! Stay above 0.")}
 	});
+    
+    $("a").on("click", function(){
+        console.log(window.location.href);
+  $(".nav").find(".active").removeClass("active");
+  $(this).parent().addClass("active");
 });
+   
+    
+});
+
+
+
