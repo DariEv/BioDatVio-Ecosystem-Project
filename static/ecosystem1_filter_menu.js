a16e2dd4cf839a966662621c22d3e82daeb89262
NiBrc <nicolas.brich@student.uni-tuebingen.de>
2019-01-20 16:25:58 +0100

    Fixed filter-menu highlight bug

--- static/ecosystem1_filter_menu.js
+++ static/ecosystem1_filter_menu.js
@@ -1,25 +1,25 @@
 /* Project ecosystem 1 by Daria Evseeva, Eduardo Vela, Nicolas Brich, Sarah Ertel, Constantin Holzapfel 21.1.19 */
 
 $( document ).ready(function() {
-    
-    
+
+
     if (window.location.href == "http://127.0.0.1:5000/project") {
-        
+
         $("#proj").addClass("active")
-        
+
     } else if (window.location.href == "http://127.0.0.1:5000/dataExploration") {
-        
+
           $("#prof").addClass("active")
-        
+
     } else if (window.location.href == "http://127.0.0.1:5000/metadataOverview") {
-        
+
           $("#meta").addClass("active")
-        
+
     } else if (window.location.href == "http://127.0.0.1:5000/PCoA"){
-        
+
           $("#pcoa").addClass("active")
     }
-    
+
     //filter
     $(".dropdown-menu li a").on('click', function() {
 		$(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
@@ -37,15 +37,11 @@ $( document ).ready(function() {
 		}
 		else{alert("Out of range! Stay above 0.")}
 	});
-    
-    $("a").on("click", function(){
-        console.log(window.location.href);
-  $(".nav").find(".active").removeClass("active");
+
+  $("a").on("click", function(){
+  $($(this).parent().parent()).find('.active').removeClass('active');
   $(this).parent().addClass("active");
 });
-   
-    
+
+
 });
-
-
-
